0bbea8a5075bcc447a182e04293aaf84ba924a1b
imkaushikgoswami <goswamikaushik2003@gmail.com>
2023-09-10 21:32:07 +0530

    create Destroy method for delete post

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+//require method-override
+const methodOverride = require("method-override");
+
+
 //require uuid package
 const {v4 : uuidv4 } = require("uuid");
 // we are adding this package and make function uuidv4
 
 app.use(express.urlencoded({extended : true }));
+app.use(methodOverride("_method"));
 
 //set view engine for ejs files
 app.set("view engine", "ejs");
@@ -66,13 +71,19 @@ app.patch("/posts/:id", (req, res) => {
     let newContent = req.body.content;
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
-    res.send("patch request");
+    res.redirect("/posts")
 })
 
 app.get("/posts/:id/edit", (req, res) => {
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+     posts = posts.filter((p) => id != p.id);
+    res.redirect("/posts")
 });
 
 app.listen(port, () => {
