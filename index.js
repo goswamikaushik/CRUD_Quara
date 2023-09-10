4b6b4f13f03e52ca77ca54f9fd9c5af14aab8209
imkaushikgoswami <goswamikaushik2003@gmail.com>
2023-09-10 17:06:37 +0530

    adding new package uuid that is used for genrating unique id for our use and also same chnages in index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+//require uuid package
+const {v4 : uuidv4 } = require("uuid");
+// we are adding this package and make function uuidv4
 
 app.use(express.urlencoded({extended : true }));
 
@@ -17,17 +20,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id : "1a",
+        id : uuidv4(),
         username : "apnacollege",
         content :"Object-Oriented Programming (OOP) is a programming paradigm based on the concept of objects, which are instances of classes. It promotes the organization of code into reusable and self-contained units (objects) that encapsulate both data and behavior. OOP principles include encapsulation, inheritance, polymorphism, classes, and objects, enabling modular and structured software development."
     },
     {
-        id : "2b",
+        id : uuidv4(),
         username : "Kaushik",
         content :" callback is a function or piece of code that is passed as an argument to another function, and it is expected to be executed at a later time. Callbacks are commonly used in programming to enable asynchronous or event-driven behavior, allowing you to define what should happen when a specific event or task is completed. Callbacks are a fundamental concept in many programming languages and are often used in scenarios like handling user input, responding to network requests, or managing timers and events."
     },
     {
-        id : "3c",
+        id : uuidv4(),
         username : "Manav",
         content :"An arrow function, also known as a fat arrow function, is a concise way to write functions in JavaScript. It was introduced in ECMAScript 6 (ES6) and provides a more compact syntax compared to traditional function expressions. Arrow functions are particularly useful for writing short, anonymous functions or for functions where the value of this is important."
     },
@@ -45,7 +48,8 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let {username, content } = req.body;
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({ id,username, content });
     res.redirect("/posts");
 });
 
